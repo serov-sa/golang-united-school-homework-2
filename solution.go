cc3a104b621ecf239d9b025afaea40af68b4ac8b
Sergei_Serov <Sergei_Serov@epam.com>
2022-02-22 19:08:46 +0400

    Homework-2

--- solution.go
+++ solution.go
@@ -1,5 +1,10 @@
 package square
 
+import (
+	"fmt"
+	"math"
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +13,30 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type CustomType int
+
+const (
+	SideCircle   = iota
+	SideTriangle = iota + 2
+	SideSquare   = iota + 2
+)
+
+func CalcSquare(sideLen float64, sidesNum CustomType) float64 {
+
+	if sideLen == 0 {
+		fmt.Println("You need to specify the length of the side of the figure or the length of the radius of the circle")
+		return 0
+	}
+
+	switch sidesNum {
+	case SideSquare:
+		return math.Pow(sideLen, 2)
+	case SideTriangle:
+		return ((math.Pow(sideLen, 2)) * math.Sqrt(3)) / 4
+	case SideCircle:
+		return math.Pow(sideLen, 2) * math.Pi
+	default:
+		fmt.Println("The sides are incorrect. Available options: 4, 3, 0")
+		return 0
+	}
 }
